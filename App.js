65531d0e96b46e8835b9aadcbe8574903076c8d5
ADESSO\eyupoglu <Mehmet.Eyupoglu@agesa.com.tr>
2025-06-18 13:01:21 +0300

    feat: start new real app

--- App.js
+++ App.js
@@ -1,58 +1,20 @@
-import { useState } from 'react';
-import { StyleSheet, View, FlatList, Button } from 'react-native';
-import GoalItem from './components/GoalItem';
-import GoalInput from './components/GoalInput';
 import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [modalIsVisible, setModalIsVisible] = useState(false)
-  const [courseGoals, setCourseGoals] = useState([]);
-
-  const addGoalHandler = (enteredGoalText) => {
-    setCourseGoals((currentCourseGoals) => [...currentCourseGoals, enteredGoalText])
-    endAddGoalHandler()
-  }
-
-  const startAddGoalHandler = () => {
-    setModalIsVisible(true)
-  }
-
-  const endAddGoalHandler = () => {
-    setModalIsVisible(false)
-  }
-
-  const deleteGoalHandler = (id) => {
-    setCourseGoals(currentCourseGoals => {
-      return currentCourseGoals.filter((goal) => goal.index === id)
-    })
-  }
-
   return (
-    <>
-      <View style={styles.appContainer}>
-        <Button title='Add New Goal' color={'#5e0acc'} onPress={startAddGoalHandler}/>
-        {modalIsVisible && <GoalInput onAddGoal={addGoalHandler} onCancel={endAddGoalHandler}/>}
-        
-        <View style={styles.goalsContainer}>
-          <FlatList data={courseGoals} renderItem={(itemData) => {
-            return (
-              <GoalItem itemData={itemData} id={itemData.index} onDeleteItem={deleteGoalHandler}/>
-            )
-          }} />
-        </View>
-      </View>
-    </>
+    <View style={styles.container}>
+      <Text>Hello World!!!!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  appContainer: {
+  container: {
     flex: 1,
-    padding: 50,
-    paddingHorizontal: 16,
-    // backgroundColor: 'blue'
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  goalsContainer: {
-    flex: 4
-  }
 });
